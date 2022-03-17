a1cee5113f49d9a694c5c67a3c5720a41b3bc426
abersheeran <me@abersheeran.com>
2022-03-17 02:17:20 +0000

    black .

--- script/upload.py
+++ script/upload.py
@@ -19,7 +19,9 @@ def get_version(package: str = package_name) -> str:
 
 os.chdir(here)
 subprocess.check_call(f"poetry version {get_version()}", shell=True)
-subprocess.check_call(f"git add {package_name}/__version__.py pyproject.toml", shell=True)
+subprocess.check_call(
+    f"git add {package_name}/__version__.py pyproject.toml", shell=True
+)
 subprocess.check_call(f'git commit -m "v{get_version()}"', shell=True)
 subprocess.check_call("git push", shell=True)
 subprocess.check_call("git tag v{0}".format(get_version()), shell=True)
